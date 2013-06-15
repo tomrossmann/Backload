2b259081ee0424559bef3f86692ba23cdfc47ea5
Georg Auersberg <geoutility@freenet.de>
2013-06-15 17:11:03 +0200

    Created: Default samples by blueimp for  Backload
    These are the original demos from the jQuery File Upload project page
    setup for working with the Backload component and in MVC style ;-).

---
+++ Examples/Demos_by_bluimp/App_Start/BackloadConfig.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Backload.Demos.BackloadMVC.AppStart_Initializer), "Initialize")]
+ 
+namespace Backload.Demos.BackloadMVC {
+    public static class AppStart_Initializer {
+        public static void Initialize() {
+            // Use bundeling for client files (scripts (js) and styles (css)). 
+            // Comment this out, if you manually include the files in your page
+            // Overload: RegisterBundles(BundleTable.Bundles, pathToScripts, pathToStyles) 
+            // Example: Backload.Configuration.FileUploadBundles.RegisterBundles(BundleTable.Bundles, "~/Scripts/FileUpload/", "~/Content/FileUpload/css/");
+            Backload.Configuration.FileUploadBundles.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Examples/Demos_by_bluimp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Backload.Demos.MvcApplication" Language="C#" %>
