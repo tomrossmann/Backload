c9373ced7acea71960020bd02b3dfdeedf31d9ac
Georg Auersberg <geoutility@freenet.de>
2013-06-24 23:55:37 +0200

    Added Example 08: Extensions

---
+++ Examples/Example08/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿//using Backload.Examples.Example08.Models;
+using System.Web.Mvc;
+using System.Linq;
+using Backload.Examples.Example08.Data;
+using Backload.Examples.Example08.Data.Models;
+using System.Collections.Generic;
+
+namespace Backload.Examples.Example08.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var artists = new ArtistsLibrary())
+            {
+                return View(artists.Artists.ToList<Artist>());
+            }
+        }
+    }
+}
---
+++ Examples/Example08/DataLayer/ArtistsLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Backload.Examples.Example08.Data.Models;
+
+namespace Backload.Examples.Example08.Data
+{
+
+    public class ArtistsLibrary : DbContext
+    {
+        public ArtistsLibrary()
+            : base("name=ArtistsLibrary")
+        {
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+
+    }
+}
