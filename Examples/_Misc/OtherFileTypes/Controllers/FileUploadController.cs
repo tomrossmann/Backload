ed0e29e549ece15107084c2daf8f9b6c25873e9a
Georg Auersberg <geoutility@freenet.de>
2013-07-31 23:27:56 +0200

    Misc test project

---
+++ Examples/_Misc/OtherFileTypes/Controllers/FileUploadController.cs
@@ -0,0 +1,42 @@
+﻿using Backload.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Backload;
+using System.Threading.Tasks;
+
+namespace Backload.Demo.OtherFileTypes.Controllers
+{
+
+    public class FileUploadController : Controller
+    {
+        public async Task<ActionResult> FileHandler()
+        {
+            FileUploadHandler handler = new FileUploadHandler(Request, this);
+            handler.FileUploadedStarted += FileUploadedStarted;
+            handler.FileUploadedFinished += handler_FileUploadedFinished;
+            handler.FileUploadException +=handler_FileUploadException;
+            ActionResult result = await handler.HandleRequestAsync();
+
+            return result;
+        }
+
+        void FileUploadedStarted(object sender, Backload.Eventing.UploadStartedEventArgs e)
+        {
+            // do something
+        }
+        
+        void handler_FileUploadException(object sender, Eventing.UploadExceptionEventArgs e)
+        {
+            // do something
+        }
+
+        void handler_FileUploadedFinished(object sender, Eventing.UploadFinishedEventArgs e)
+        {
+            // error handling
+        }
+    }
+}
---
+++ Examples/_Misc/OtherFileTypes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Backload.Demo.OtherFileTypes.MvcApplication" Language="C#" %>
