552442dcc6f9dcf8a41c8bccd58f4c2409012b90
Georg Auersberg <geoutility@freenet.de>
2013-08-09 15:52:18 +0200

    None image file type example with mapping (issue #12)

--- Examples/_Misc/OtherFileTypes/Controllers/FileUploadController.cs
+++ Examples/_Misc/OtherFileTypes/Controllers/FileUploadController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using Backload;
 using System.Threading.Tasks;
+using Backload.Eventing.Args;
 
 namespace Backload.Demo.OtherFileTypes.Controllers
 {
@@ -19,25 +20,39 @@ namespace Backload.Demo.OtherFileTypes.Controllers
             handler.StoreFileRequestStarted += handler_StoreFileRequestStarted;
             handler.StoreFileRequestFinished += handler_StoreFileRequestFinished;
             handler.StoreFileRequestException += handler_StoreFileRequestException;
-            ActionResult result = await handler.HandleRequestAsync();
+            handler.OutgoingResponseCreated += handler_OutgoingResponseCreated;
+            handler.ProcessPipelineExceptionOccured += handler_ProcessPipelineExceptionOccured;
 
+            ActionResult result = await handler.HandleRequestAsync();
             return result;
         }
 
-
-        void handler_StoreFileRequestStarted(object sender, Eventing.Args.StoreFileRequestEventArgs e)
+        void handler_StoreFileRequestStarted(object sender, StoreFileRequestEventArgs e)
         {
             // do something
         }
 
-        void handler_StoreFileRequestFinished(object sender, Eventing.Args.StoreFileRequestEventArgs e)
+        void handler_StoreFileRequestFinished(object sender, StoreFileRequestEventArgs e)
         {
-            // do something
+            var status = e.Param.FileStatusItem;
         }
 
-        void handler_StoreFileRequestException(object sender, Eventing.Args.StoreFileRequestEventArgs e)
+        void handler_StoreFileRequestException(object sender, StoreFileRequestEventArgs e)
         {
-            // do something
+            var exception = e.Param.FileStatusItem.Exception;
+        }
+
+        void handler_OutgoingResponseCreated(object sender, OutgoingResponseEventArgs e)
+        {
+            if (e.Context.RequestType == Contracts.Context.RequestType.Default)
+            {
+                JsonResult result = (JsonResult)e.Param.Result;
+            }
+        }
+
+        void handler_ProcessPipelineExceptionOccured(object sender, ProcessPipelineExceptionEventArgs e)
+        {
+            var files = e.Param.Exception;
         }
     }
 }
